9da7119788d4807d1e2980e9c81fc2f555341333
areddy548 <janardhananipireddy333@gmail.com>
2025-09-25 04:41:03 -0400

    user profile changes

--- secondChance-backend/routes/authRoutes.js
+++ secondChance-backend/routes/authRoutes.js
@@ -4,6 +4,7 @@ const connectToDatabase = require('../models/db');
 const logger = require('../logger');
 const bcryptjs = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const { body, validationResult } = require('express-validator');
 
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret_key'
 
@@ -86,6 +87,65 @@ router.post('/login', async (req, res) => {
     } catch (e) {
          return res.status(500).send('Internal server error');
 
+    }
+});
+
+router.put('/update', async (req, res) => {
+        // Task 2: Validate the input using `validationResult` and return an appropriate message if you detect an error
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+    logger.error('Validation errors in update request', errors.array());
+    return res.status(400).json({ errors: errors.array() });
+    }
+    try {
+        // Task 3: Check if `email` is present in the header and throw an appropriate error message if it is not present
+        const email = req.headers.email;
+
+        if (!email) {
+            logger.error('Email not found in the request headers');
+            return res.status(400).json({ error: "Email not found in the request headers" });
+        }
+        // Task 4: Connect to MongoDB
+        const db = await connectToDatabase();
+        const collection = db.collection("users");
+        // Task 5: Find the user credentials in database
+        const existingUser = await collection.findOne({ email });
+        
+        if (!existingUser) {
+        logger.error('User not found');
+        return res.status(404).json({ error: 'User not found' });
+        }
+
+        existingUser.updatedAt = new Date();
+        // Task 6: Update the user credentials in the database
+        
+        const updateFields = {
+        ...req.body,
+        updatedAt: new Date(),
+        };
+
+        const updatedUser = await collection.findOneAndUpdate(
+            { email },
+            { $set: updateFields },
+            { returnDocument: 'after' }
+        );
+
+        if (!updatedUser.value) {
+        logger.error('User not found during update');
+        return res.status(404).json({ error: 'User not found' });
+        }
+
+        // Task 7: Create JWT authentication with `user._id` as a payload using the secret key from the .env file
+        const payload = {
+            user: {
+            id: updatedUser.value._id.toString(),
+            },
+        };
+
+        const authtoken = jwt.sign(payload, JWT_SECRET);
+        res.json({authtoken});
+    } catch (e) {
+         return res.status(500).send('Internal server error');
     }
 });
 
